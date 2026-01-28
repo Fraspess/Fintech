832b7d93fc5d2cedb733f5b2d0cbbf9d217226d4
CODER <CODER@rivne.itstep.org>
2026-01-28 18:02:46 +0200

    started

---
+++ webmvc/src/main/java/org/example/services/CourseService.java
@@ -0,0 +1,16 @@
+package org.example.services;
+
+import java.util.List;
+
+import org.example.models.Course;
+import org.springframework.data.domain.Page;
+
+public interface CourseService {
+    List<Course> getAllCourses();
+    void saveCourse(Course course);
+    Course getCourseById(long id);
+    void deleteCourseById(long id);
+    Page<Course> findPaginated(int pageNum, int pageSize,
+                               String sortField,
+                               String sortDirection);
+}
