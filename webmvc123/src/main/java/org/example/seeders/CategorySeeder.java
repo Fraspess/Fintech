f72bdf0686c3cde42d15b1d411c58452b31da45a
Fraspess <pavlosamchyk@gmail.com>
2026-02-02 17:56:02 +0200

    Added seeder

---
+++ webmvc123/src/main/java/org/example/seeders/CategorySeeder.java
@@ -0,0 +1,40 @@
+package org.example.seeders;
+
+import com.github.slugify.Slugify;
+import org.example.entities.CategoryEntity;
+import org.example.repositories.ICategoryRepository;
+import org.example.services.FileService;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CategorySeeder implements CommandLineRunner {
+
+    private final ICategoryRepository categoryRepository;
+    private final Slugify slg = Slugify.builder().transliterator(true).build();
+    private final FileService fileService;
+
+    public CategorySeeder(ICategoryRepository categoryRepository, FileService fileService) {
+        this.categoryRepository = categoryRepository;
+        this.fileService = fileService;
+    }
+
+    @Override
+    public void run(String... args) {
+        if(categoryRepository.count() == 0){
+            CategoryEntity category1 = new CategoryEntity();
+            category1.setName("Сало");
+            category1.setSlug(slg.slugify(category1.getName()));
+            category1.setImage(fileService.load("https://klopotenko.com/wp-content/uploads/2025/04/salo-varene-img-1000x600.jpg?v=1743596316"));
+
+            CategoryEntity category2 = new CategoryEntity();
+            category2.setName("Морозиво");
+            category2.setSlug(slg.slugify(category2.getName()));
+            category2.setImage(fileService.load("https://profsystems.com.ua/images/stories/equipment/ice-cream/soft.jpg"));
+
+
+            categoryRepository.save(category1);
+            categoryRepository.save(category2);
+        }
+    }
+}
